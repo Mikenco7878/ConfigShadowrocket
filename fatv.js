aaf94117cf7bf852c667f2fa86dc2004590d001d
Lam Dien <dienplce171407@fpt.edu.vn>
2026-07-14 20:27:56 +0700

    Organize modules and add Shadowrocket config

--- fatv.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body);
-
-obj.role = 2;
-obj.sync = "2027-02-28T06:31:27.761Z";
-obj.noAdDate = "2099-09-09T19:40:44.053Z";
-obj.noAdDate_Adult = "2099-09-09T19:40:44.053Z";
-obj.freeTime = 0;
-
-$done({body: JSON.stringify(obj)}); 
